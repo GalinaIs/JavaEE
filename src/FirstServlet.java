3cbcdf09fe1306ac5c5fa804d139d716b16aedd5
Galina Is <denis@isyanov.com>
2020-02-08 12:30:25 +0300

    lesson 8 cart session

--- src/FirstServlet.java
+++ src/FirstServlet.java
@@ -1,25 +1,26 @@
-import javax.servlet.RequestDispatcher;
+import logic.Cart;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class FirstServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
-        Integer count = (Integer) session.getAttribute("count");
-        if (count == null) {
-            count = 0;
+        String name = req.getParameter("name");
+        int quantity = Integer.parseInt(req.getParameter("quantity"));
+        Cart cart = (Cart) session.getAttribute("cart");
+        if (cart == null) {
+            cart = new Cart();
+            cart.setName(name);
+            cart.setQuantity(quantity);
         }
-        session.setAttribute("count", ++count);
-        PrintWriter printWriter = resp.getWriter();
-        printWriter.println("<html>");
-        printWriter.println("<h1>Your count is " + count+ "</h1>");
-        printWriter.println("</html>");
+        session.setAttribute("cart", cart);
+        getServletContext().getRequestDispatcher("/showCard.jsp").forward(req, resp);
     }
 
     @Override
